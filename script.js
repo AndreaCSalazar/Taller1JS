d70978f8a10ef1581e6beb1f2a36c1719cca98b2
Julieth A Ballesteros O <juliethalejandrayuyis@gmail.com>
2024-09-08 19:18:07 -0500

    Se agregó el código js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
+    let listaDeNotas = [];
+    let inputTexto = document.getElementById("descriptionId");
+    let botonEnviar = document.querySelector(".btn");
+    let botonVerNotas = document.querySelector(".btn2");
 
-    $form.addEventListener("submit", (Event) =>{
-        Event.preventDefault();
-        addNote();
+    botonEnviar.addEventListener("click", event => {
+        event.preventDefault();
+
+        listaDeNotas.push(inputTexto.value);
+        inputTexto.value = "";
+
+        console.log(listaDeNotas)
     });
+
+
+    botonVerNotas.addEventListener("click", event => {
+        event.preventDefault();
+
+        let todasLasNotas = "";
+        
+        listaDeNotas.forEach(nota => {
+            todasLasNotas += nota + "\n"
+        })
+
+        alert(todasLasNotas)
+    })
 });
