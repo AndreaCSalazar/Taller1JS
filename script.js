1a1ce87a1ae80746fade82af01c00ade34615a9e
Andrea Contreras <tialy.28@gmail.com>
2024-09-25 15:37:22 -0500

    taller1 version2

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
+    let listNotes = [];
+    let inputText = document.getElementById("descriptionId");
+    let seeNotesBtn = document.querySelector(".btn");
+
+    let $form = document.querySelector('form');
 
     $form.addEventListener("submit", (Event) =>{
         Event.preventDefault();
         addNote();
+    });
+
+    function addNote() {
+        let note = inputText.value();
+        if (note) {
+            listNotes.push(note);
+            inputText.value = "";
+            console.log(listNotes);
+        } else {
+            alert("Please write something in the description!");
+        }
+    }
+
+    
+
+    seeNotesBtn.addEventListener("click", event => {
+        event.preventDefault();
+
+        let allNotes = "";
+
+        listNotes.forEach(note => {
+            allNotes += note + "\n"
+        })
+
+        alert(allNotes);
+        
     });
 });
